60413d4c5fcb41bece4dde4d7c668394d3d45945
younwony <wony9324@naver.com>
2021-05-21 13:42:00 +0900

    Initial commit

---
+++ src/main/java/dev/wony/privatestudio/PrivateStudioApplication.java
@@ -0,0 +1,13 @@
+package dev.wony.privatestudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrivateStudioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrivateStudioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/wony/privatestudio/PrivateStudioApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.wony.privatestudio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrivateStudioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
